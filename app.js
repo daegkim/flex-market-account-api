37deb40545082bee27feb6d6123d8d05dd7487ae
daegeunkim <daegeunkim92@gmail.com>
2021-07-22 22:25:25 +0900

    쓸일없는 프로젝트 정리

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var path = require('path');
 var createError = require('http-errors');
 
 var accountRouter = require('./routes/account');
@@ -10,14 +9,10 @@ var accountRouter = require('./routes/account');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', accountRouter);
 
@@ -28,11 +23,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send('error');
 });
 
 //서버 실행과 동시에 mongodb 접속
